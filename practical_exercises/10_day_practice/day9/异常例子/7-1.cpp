76ab8b61db2b87359a93e6b7c7e0ffda8d8c2e5c
Falling-in-W-sweetty <63394589+Falling-in-W-sweetty@users.noreply.github.com>
2022-08-12 18:12:04 +0800

    Update 7-1.cpp

--- practical_exercises/10_day_practice/day9/异常例子/7-1.cpp
+++ practical_exercises/10_day_practice/day9/异常例子/7-1.cpp
@@ -1,4 +1,7 @@
 //Eg10-9.cpp
+//有可能产生两种报告方式
+//　　1、抛出bad_alloc异常来报告分配失败；
+//    2、返回空指针，而不会抛出异常。
 #include<iostream>
 using namespace std;
 class A{
@@ -14,7 +17,7 @@ class B{
         B(int k){
             cout<<"int B constructor..."<<endl;
             for (int i=0;i<10;i++){
-                pb[i]=new double[20000000];
+                pb[i]=new double[20000000];//第一种方式抛出异常时，后面的程序不会执行，这段程序只能针对方式2
                 if(pb[i]==0)
                     throw i;
                 else 
